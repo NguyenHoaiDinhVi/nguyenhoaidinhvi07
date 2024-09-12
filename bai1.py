7d50d27909c74587dbd24458decc7ede20afd694
NguyenHoaiDinhVi <tudue610@gmail.com>
2024-09-12 19:15:02 +0700

    Add files via upload

---
+++ bai1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 12 18:11:17 2024
+
+@author: Student
+"""
+T=1
+n=int(input("n="))
+for i in range(1,n+1):
+    T= T*i
+print("giai thừa của",n,"là:",T)
+    
